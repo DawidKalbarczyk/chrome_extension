c974cb6d3070c6f1474dae51fe99b647524b3dcc
Dawid <u56128@student.wat.edu.pl>
2026-03-12 11:00:32 +0100

    Sending changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("button-yes").addEventListener("click", () => {
     document.querySelector("h1").textContent = "You clicked Yes!";
+    document.querySelectorAll(".p").forEach((element) => {
+    const fontSize = parseInt(window.getComputedStyle(element).fontSize);
+    element.style.fontSize = fontSize * 4 + "px";
 });
+
+});
+
