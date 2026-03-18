faf568a8b8ccb46150773157b45ee24e3500b17b
Dawid Kalbarczyk <u56128@student.wat.edu.pl>
2026-03-18 07:13:14 +0100

    Yes

--- content.js
+++ content.js
@@ -1,26 +1,32 @@
-function renderReadingTime(article) {
-  // If we weren't provided an article, we don't need to render anything.
-  if (!article) {
-    return;
-  }
-
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+function renderBiggerFont(fontSizeIncrement) {
+    
+    const list = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "span", "a", "li", "div", "button", "label"];
+    for (let i = 0; i < list.length; i++) {
+        document.querySelectorAll(` ${list[i]}`).forEach(element => {
+            element.style.fontSize = (parseInt(window.getComputedStyle(element).fontSize) + fontSizeIncrement) + "px";
+        });
+    }
 }
 
-renderReadingTime(document.querySelector("article"));
+function primeFontSize() {
+    
+    document.querySelectorAll(`p, h1, h2, h3, h4, h5, h6, span, a, li, div, button, label`).forEach((element) => {
+        const tagName = element.tagName.toLowerCase();
+        const fontSize = parseInt(window.getComputedStyle(element).fontSize);
+        dict[tagName].push(fontSize);
+    });
+    return dict;
+}
+
+chrome.storage.local.get(["sliderValue", "isResizing"], (data) => {
+    if (data.isResizing && data.sliderValue) {
+        renderBiggerFont(parseInt(data.sliderValue));
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "renderBiggerFont") {
+        renderBiggerFont(request.fontSizeIncrement);
+        sendResponse({ status: "Czcionka zmieniona!" });
+    }
+});
--- script.js
+++ script.js
@@ -1,9 +1,60 @@
+let sliderValue = parseInt(localStorage.getItem("sliderValue")) || 1;
+let isResizing = parseInt(localStorage.getItem("isResizing")) || false;
+let lastIncrement = 0;
+let primalFontSize = [];
+//Dodaj localstorage do przechowywania wartości pierwotnej czcionki dla każdego document.querySelectorAll(`p, h1, h2, h3, h4, h5, h6, span, a, li, div, button, label`).forEach((element) => {
+//Nie działa dla innych stro niż testowana
+
+document.getElementById("slider-id").value = sliderValue;
+document.getElementById("slider-value").textContent = sliderValue;
+
+function renderFunction() {
+    chrome.sendMessage(tabs[0].id, {
+        action: "primeFontSize"
+    });
+    if (lastIncrement === sliderValue) {
+        return;
+    } else {
+        lastIncrement = sliderValue;
+    }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { 
+            action: "renderBiggerFont",
+            fontSizeIncrement: parseInt(sliderValue),
+            
+        }, (response) => {
+            console.log("Response from content script:", response);
+        });
+    });
+}
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { 
+        action: "getSliderValue",
+        fontSizeIncrement: parseInt(sliderValue) || 0
+    }, (response) => {
+        console.log("Response from content script:", response);
+    });
+});
+
+
+
+document.getElementById("slider-id").addEventListener("input", (event) => {
+    sliderValue = event.target.value;
+    document.getElementById("slider-value").textContent = sliderValue;
+    localStorage.setItem("sliderValue", sliderValue);
+    chrome.storage.local.set({ sliderValue: parseInt(sliderValue) });
+});
+
+
 document.getElementById("button-yes").addEventListener("click", () => {
     document.querySelector("h1").textContent = "You clicked Yes!";
-    document.querySelectorAll(".p").forEach((element) => {
-    const fontSize = parseInt(window.getComputedStyle(element).fontSize);
-    element.style.fontSize = fontSize * 4 + "px";
+    localStorage.setItem("isResizing", true);
+    chrome.storage.local.set({ isResizing: true, sliderValue: parseInt(sliderValue) });
+    renderFunction();
 });
 
+document.getElementById("button-no").addEventListener("click", () => {
+    document.querySelector("h1").textContent = "You clicked No!";
+    localStorage.setItem("isResizing", false);
+    renderFunction();
 });
-
